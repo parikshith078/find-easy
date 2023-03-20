d82a0cfb9812fabc2182c730ebb7bb3c28e983cf
parikshith078 <palegar.parikshith@gmail.com>
2023-03-20 19:06:26 +0530

    bug fix

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/header/Header";
-import styles from "@/styles/Login.module.css"
+import Header from "@/components/Header";
+import styles from "@/styles/Login.module.css";
 
 const LoginPage: React.FC = () => {
   const signInWithGoogle = () => {
@@ -7,35 +7,49 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-
     <div className="wrapper">
       {/* <Header /> */}
-<div className={styles.centerDiv}>
-      <div className={styles.main}>
-      <h1>Login </h1>
-      <div className={styles.loginForm}>
-        <label htmlFor="email">Email/Username:</label>
-        <input className={styles.inputField} type="text" placeholder="Enter email" id="email" name="email" required />
-        <label htmlFor="password">Password:</label>
-        <input type="password" className={styles.inputField} placeholder="Enter password" id="password" name="password" required />
-        <label htmlFor="remember-me">
-        <input  type="checkbox" id="remember-me" name="remember-me" />
-          Remember me
-        </label>
-        <a href="#">Forgot Password?</a>
-        <div  className={styles.loginBtn} >Login</div>
-       
-      </div>
-      <div className={styles.googleBtn} onClick={signInWithGoogle}>
-          <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Icon" />
-          <span>Continue with Google</span>
+      <div className={styles.centerDiv}>
+        <div className={styles.main}>
+          <h1>Login </h1>
+          <div className={styles.loginForm}>
+            <label htmlFor="email">Email/Username:</label>
+            <input
+              className={styles.inputField}
+              type="text"
+              placeholder="Enter email"
+              id="email"
+              name="email"
+              required
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              className={styles.inputField}
+              placeholder="Enter password"
+              id="password"
+              name="password"
+              required
+            />
+            <label htmlFor="remember-me">
+              <input type="checkbox" id="remember-me" name="remember-me" />
+              Remember me
+            </label>
+            <a href="#">Forgot Password?</a>
+            <div className={styles.loginBtn}>Login</div>
+          </div>
+          <div className={styles.googleBtn} onClick={signInWithGoogle}>
+            <img
+              src="https://img.icons8.com/color/48/000000/google-logo.png"
+              alt="Google Icon"
+            />
+            <span>Continue with Google</span>
+          </div>
+          {/* <p>New to FIND-EASY? <a href="#">Sign up</a> now!</p> */}
         </div>
-      {/* <p>New to FIND-EASY? <a href="#">Sign up</a> now!</p> */}
-    </div>
-    </div>
+      </div>
     </div>
   );
 };
 
 export default LoginPage;
-
