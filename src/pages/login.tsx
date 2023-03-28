55a5164159cb6ca42e7445191768dfbe56b3a595
Avinav-kashyap04 <avinav04kashyap@gmail.com>
2023-03-28 20:28:42 +0530

    fixed the image bug

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,29 +10,29 @@ const LoginPage: React.FC = () => {
 
     <div className="wrapper">
       {/* <Header /> */}
-<div className={styles.centerDiv}>
-      <div className={styles.main}>
-      <h1>Login </h1>
-      <div className={styles.loginForm}>
-        <label htmlFor="email">Email/Username:</label>
-        <input className={styles.inputField} type="text" placeholder="Enter email" id="email" name="email" required />
-        <label htmlFor="password">Password:</label>
-        <input type="password" className={styles.inputField} placeholder="Enter password" id="password" name="password" required />
-        <label htmlFor="remember-me">
-        <input  type="checkbox" id="remember-me" name="remember-me" />
-          Remember me
-        </label>
-        <a href="#">Forgot Password?</a>
-        <div  className={styles.loginBtn} >Login</div>
-       
-      </div>
-      <div className={styles.googleBtn} onClick={signInWithGoogle}>
-          <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Icon" />
-          <span>Continue with Google</span>
+      <div className={styles.centerDiv}>
+        <div className={styles.main}>
+          <h1>Login </h1>
+          <div className={styles.loginForm}>
+            <label htmlFor="email">Email/Username:</label>
+            <input className={styles.inputField} type="text" placeholder="Enter email" id="email" name="email" required />
+            <label htmlFor="password">Password:</label>
+            <input type="password" className={styles.inputField} placeholder="Enter password" id="password" name="password" required />
+            <label htmlFor="remember-me">
+              <input type="checkbox" id="remember-me" name="remember-me" />
+              Remember me
+            </label>
+            <a href="#">Forgot Password?</a>
+            <div className={styles.loginBtn} >Login</div>
+
+          </div>
+          <div className={styles.googleBtn} onClick={signInWithGoogle}>
+            <picture><img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Icon" /></picture>
+            <span>Continue with Google</span>
+          </div>
+          {/* <p>New to FIND-EASY? <a href="#">Sign up</a> now!</p> */}
         </div>
-      {/* <p>New to FIND-EASY? <a href="#">Sign up</a> now!</p> */}
-    </div>
-    </div>
+      </div>
     </div>
   );
 };
