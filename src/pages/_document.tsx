cd26d1afe4365e0ff7ee88af60e8cae6130b7f5b
parikshith078 <palegar.parikshith@gmail.com>
2023-04-15 12:15:13 +0530

    fixing font issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="scroll-smooth" data-theme="cupcake">
+    <Html lang="en" className="scroll-smooth poppins " data-theme="cupcake">
       <Head />
       <body>
         <Main />
