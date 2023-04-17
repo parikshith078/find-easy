fe4fb89e410e028aa00ab99845d585104958b0f3
shashank-g2100 <97581841+shashank-g2100@users.noreply.github.com>
2023-04-17 17:00:25 +0530

    product page dynamic routing

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,109 +1,7 @@
-// import { MailService } from '@sendgrid/mail/src/mail';
-// import twilio from 'twilio';
-// import sgMail from '@sendgrid/mail';
-
-// export default async function handler(req: any, res: any) {
-//   const accountSid = 'AC9bafb65aff4eecfd5eca2a761ed78982';
-//   const authToken = '8b4d35d24ec0fe70f18f38210d6df098';
-//   const client = twilio(accountSid, authToken);
-
-//   try {
-//     const message = await client.messages.create({
-//       body: 'Hello from Twilio!',
-//       from: '+14345776835', 
-//       to: '+917349012319', 
-//     });
-
-//     console.log(message.sid);
-//     res.status(200).send('SMS sent successfully');
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Error sending SMS');
-//   }
-// }
-
-// const sgMail = require ('@sendgrid/mail')
-
-// const API_KEY = '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';
-
-// sgMail.setApiKey(API_KEY)
-
-// const message = {
-//   to: 'shashank.g2100@gmail.com',
-//   from: '21bcs107@iiitdwd.ac.in',
-
-// }
 
 // import twilio from 'twilio';
 // import sgMail from '@sendgrid/mail';
-
-// const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// twilioClient.messages.create({
-//   body: 'This is a test SMS sent using Twilio!',
-//   from: '+14345776835', // Your Twilio phone number
-//   to: '+917349012319', // The recipient's phone number
-// })
-// .then(() => {
-//   console.log('SMS sent');
-// })
-// .catch((error) => {
-//   console.error(error);
-// });
-
-// const msg = {
-//   to: 'recipient@example.com',
-//   from: 'sender@example.com',
-//   subject: 'Test email',
-//   text: 'This is a test email sent using Twilio and SendGrid!',
-//   html: '<p>This is a test email sent using Twilio and SendGrid!</p>',
-// };
-
-// sgMail.send(msg)
-//   .then(() => {
-//     console.log('Email sent');
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
-// import twilio from 'twilio';
-// import sgMail from '@sendgrid/mail';
-
-// const twilioClient = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// twilioClient.messages.create({
-//   body: 'This is a test SMS sent using Twilio!',
-//   from: '+14345776835', // Your Twilio phone number
-//   to: '+917349012319', // The recipient's phone number
-// })
-// .then(() => {
-//   console.log('SMS sent');
-// })
-// .catch((error) => {
-//   console.error(error);
-// });
-
-// const msg = {
-//   to: 'sharath.g2100@gmail.com',
-//   from: '21bcs107@iiitdwd.ac.in',
-//   subject: 'Test email',
-//   text: 'This is a test email sent using Twilio and SendGrid!',
-//   html: '<p>This is a test email sent using Twilio and SendGrid!</p>',
-// };
-
-// sgMail.send(msg)
-//   .then(() => {
-//     console.log('Email sent');
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
-// import twilio from 'twilio';
-// import sgMail from '@sendgrid/mail';
+// import { MailDataRequired } from '@sendgrid/mail';
 // require('dotenv').config(); // Load environment variables from .env file
 
 // const twilioClient = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
@@ -137,40 +35,164 @@
 //     console.error(error);
 //   });
 
-const sgMail = require('@sendgrid/mail');
-const twilio = require('twilio');
-require('dotenv').config();
+// const sgMail = require('@sendgrid/mail');
+// const twilio = require('twilio');
+// require('dotenv').config();
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+// const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-const sendEmail = (toEmail: string, subject: string, text: string) => {
-  const msg = {
-    to: toEmail,
-    from: process.env.FROM_EMAIL_ADDRESS,
-    subject: subject,
-    text: text,
-    html: `<p>${text}</p>`
-  };
+// const sendEmail = (toEmail: string, subject: string, text: string) => {
+//   const msg = {
+//     to: toEmail,
+//     from: process.env.FROM_EMAIL_ADDRESS,
+//     subject: subject,
+//     text: text,
+//     html: `<p>${text}</p>`
+//   };
 
-  return sgMail.send(msg);
-}
+//   return sgMail.send(msg);
+// }
 
-const sendSMS = (toNumber: string, message: string) => {
-  return twilioClient.messages.create({
-    body: message,
-    from: process.env.TWILIO_PHONE_NUMBER,
-    to: process.env.RECIPIENT_PHONE_NUMBER
+// const sendSMS = (toNumber: string, message: string) => {
+//   return twilioClient.messages.create({
+//     body: message,
+//     from: process.env.TWILIO_PHONE_NUMBER,
+//     to: process.env.RECIPIENT_PHONE_NUMBER
 
-  });
-}
+//   });
+// }
 
-// Example usage
-sendEmail('recipient@example.com', 'Test Email', 'This is a test email.')
-  .then(() => console.log('Email sent.'))
-  .catch((error: any) => console.error(error));
+// // Example usage
+// sendEmail('recipient@example.com', 'Test Email', 'This is a test email.')
+//   .then(() => console.log('Email sent.'))
+//   .catch((error: any) => console.error(error));
 
-sendSMS('+1234567890', 'This is a test SMS.')
-  .then(() => console.log('SMS sent.'))
-  .catch((error: any) => console.error(error));
+// sendSMS('+1234567890', 'This is a test SMS.')
+//   .then(() => console.log('SMS sent.'))
+//   .catch((error: any) => console.error(error));
+
+// import { useState } from 'react';
+
+// export default function ContactForm() {
+//   const [name, setName] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [message, setMessage] = useState('');
+//   const [phone, setPhone] = useState('');
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState('');
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     setLoading(true);
+
+//     try {
+//       const response = await fetch('/api/send-sms', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ name, email, message, phone })
+//       });
+
+//       const data = await response.json();
+//       setLoading(false);
+
+//       if (!response.ok) {
+//         setError(data.error);
+//         return;
+//       }
+
+//       // Handle successful form submission
+//     } catch (error) {
+//       console.error(error);
+//       setError('Failed to submit form.');
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       // Render form input fields
+//     </form>
+//   );
+// }
+
+
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import twilio from 'twilio'
+
+// const accountSid = 'YOUR_ACCOUNT_SID';
+// const authToken = 'YOUR_AUTH_TOKEN';
+// const client = twilio(accountSid, authToken);
+
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const { name, email, message, phone } = req.body;
+
+//   // Send SMS message using Twilio
+//   client.messages
+//     .create({
+//       body: `New contact form submission from ${name} (${email}): ${message}`,
+//       from: 'YOUR_TWILIO_PHONE_NUMBER',
+//       to: phone
+//     })
+//     .then(() => {
+//       res.status(200).json({ success: true });
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       res.status(500).json({ error: 'Failed to send SMS message.' });
+//     });
+// }
+
+// const accountSid = 'AC9bafb65aff4eecfd5eca2a761ed78982';
+// const authToken = 'f10b4667727c0d31f19a453993c2d9ac';
+// const client = require('twilio')(accountSid, authToken);
+
+// client.messages
+//     .create({
+//         body: 'Hello Shashank, Have a  nice day',
+//         from: '+14345776835',
+//         to: '+917349012319'
+//     })
+//     .then((message: { sid: any; }) => console.log(message.sid))
+//     .done();
+
+import express, { Request, Response } from 'express';
+import twilio from 'twilio';
+
+const app = express();
+
+const accountSid: string = 'AC9bafb65aff4eecfd5eca2a761ed78982';
+const authToken: string = 'f10b4667727c0d31f19a453993c2d9ac';
+
+// app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
+
+app.post("/sendMessage", async (req: Request, res: Response) => {
+    const client = twilio(accountSid, authToken);
+    client.messages
+        .create({ 
+            body: req.body.message, 
+            from: "+14345776835", 
+            to: req.body.number 
+        })
+        .then(message => {
+            console.log(`Message sent to ${message.to}: ${message.body}`);
+            res.send(`Message sent to ${message.to}: ${message.body}`);
+        })
+        .catch(error => {
+            console.log(error);
+            res.send(`Error sending message to ${req.body.number}`);
+        });
+});
+
+app.get('/',(req: Request, res: Response) => {
+    res.render('product.tsx')
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
---
+++ src/pages/product/index.tsx
@@ -0,0 +1,191 @@
+import Header from "@/components/Header";
+import Fotter from "@/components/Fotter";
+import { MdIosShare } from "react-icons/md";
+import { AiOutlineHeart } from "react-icons/ai";
+import Image from "next/image";
+import { FC } from "react";
+import Link from "next/link";
+
+const Product: FC = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="hero min-h-screen bg-base-200 ">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+              <div className="w-[200vh] h-[80vh] overflow-hidden relative">
+                <Image
+                  src="/assets/house.jpg"
+                  fill
+                  className="object-cover w-full"
+                  alt="Image"
+                />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide4" className="btn btn-circle">
+                    ❮
+                  </a>
+                  <a href="#slide2" className="btn btn-circle">
+                    ❯
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+              <div className="w-[200vh] h-[80vh] overflow-hidden relative">
+                <Image
+                  src="/assets/home.jpg"
+                  fill
+                  className="object-cover w-full"
+                  alt="Image"
+                />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide1" className="btn btn-circle">
+                    ❮
+                  </a>
+                  <a href="#slide3" className="btn btn-circle">
+                    ❯
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+              <div className="w-[200vh] h-[80vh] overflow-hidden relative">
+                <Image
+                  src="/assets/kit.jpg"
+                  fill
+                  className="object-cover w-full"
+                  alt="Image"
+                />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide2" className="btn btn-circle">
+                    ❮
+                  </a>
+                  <a href="#slide4" className="btn btn-circle">
+                    ❯
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+              <div className="w-[200vh] h-[80vh] overflow-hidden relative">
+                <Image
+                  src="/assets/Liv.jpg"
+                  fill
+                  className="object-cover w-full"
+                  alt="Image"
+                />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide3" className="btn btn-circle">
+                    ❮
+                  </a>
+                  <a href="#slide1" className="btn btn-circle">
+                    ❯
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="pb-20">
+            <div className="text-center lg:text-left grid gap-y-0 grid-row-3 pl-10 pr-20">
+              <ul className="flex gap-10 font-light text-sm">
+                <li>
+                  <span className="inline-flex items-center gap-2">
+                    <h1 className="text-3xl font-bold font-medium ">
+                      Modern Flats and Apartments
+                    </h1>
+                  </span>
+                </li>
+                <li className="pl-6">
+                  <span className="inline-flex items-center gap-2 ">
+                    {/* <div className="flex items-center justify-center gap-4"> */}
+                    <div className="flex items-center justify-center gap-4">
+                      <AiOutlineHeart className="text-black-500 hover:text-gray-500 h-8 w-8" />
+                    </div>
+                    <div className="flex items-center justify-center gap-4">
+                      <MdIosShare className="text-black-500 hover:text-gray-500 h-8 w-8" />
+                    </div>
+                    {/* </div> */}
+                  </span>
+                </li>
+              </ul>
+
+              <h2 className="text-2xl font-italic font-normal">₹ 1.4cr </h2>
+              <h2 className="text-1xl font-italic font-light">
+                3 Bedroom | 2000 sq feet{" "}
+              </h2>
+              <h1 className="text-lg font-italic font-medium ">
+                <span>Andheri, Mumbai, India-400414</span>
+              </h1>
+              <div className="flex justify-between mt-6">
+                <button className="bg-primary rounded-full flex-5 hover:bg-primary-active cursor-pointer flex justify-center items-center shadow-inner gap-10 px-20 py-3 ">
+                  <p className="font-bold">Request a tour</p>
+                </button>
+                <button className="bg-primary rounded-full flex-5 hover:bg-primary-active cursor-pointer flex justify-center items-center shadow-inner gap-10 px-20 py-3 ">
+                  <p className="font-bold">
+                    <Link href="#footer">Contact Agent</Link>
+                  </p>
+                </button>
+              </div>
+
+              <h1 className="text-2xl font-bold pt-6 ">About this property</h1>
+              <p className="py-6">
+                Best 3 BHK Independent House for modern-day lifestyle is now
+                available for sale. Grab this 3 BHK property for sale in one of
+                Bengaluru top location, Attibele. The property price of this
+                unit is Rs 1.4 Cr. Monthly maintenance costs Rs 2000. The
+                built-up area is 2220 Square feet. There are 3 bedrooms and 3
+                bathroom.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <form action="/sendMessage" method="post">
+        <div className="pb-20">
+          <div className=" flex justify-between gap-14 w-full lg:flex-row md:flex-row xs:flex-col-reverse ">
+            <div className=" lg:block md:block sm:hidden xs:hidden w-full">
+              {" "}
+            </div>
+            <div className=" flex-col w-[50%] flex gap-2 pt-10 ">
+              <div className="form-control w-full max-w-xs"></div>
+              <p className=" text-lg font-semibold">Contact</p>
+              <div className=" flex gap-1 h-10  sm:flex-wrap ">
+                <input
+                  className="w-[70%] lg:w-[60%] border-solid rounded-sm p-3 border-[1px] border-[#C5C5C5] "
+                  type="text"
+                  id="fname"
+                  name="fname"
+                  placeholder="Text here..."
+                ></input>
+                {/* <div className=" bg-primary rounded-full border-solid flex-5 hover:bg-primary-active cursor-pointer flex justify-center items-center shadow-inner gap-10 px-6 py-3"> */}
+                <p className="text-lg font-semibold">
+                  <label htmlFor="my-modal-4" className="btn px-6 py-3">
+                    Submit
+                  </label>
+
+                  <input
+                    type="checkbox"
+                    id="my-modal-4"
+                    className="modal-toggle"
+                  />
+                  <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                    <label className="modal-box relative" htmlFor="">
+                      <h3 className="text-lg font-bold">
+                        Your Message has sent. Thanks Have a Nice day.
+                      </h3>
+                    </label>
+                  </label>
+                </p>
+                {/* </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+      <Fotter />
+    </div>
+  );
+};
+
+export default Product;
