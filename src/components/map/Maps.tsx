cf82e097b36476437e73f7177cf4aba177972f2e
jeevan-v8 <121881833+jeevan-v8@users.noreply.github.com>
2023-04-19 09:10:53 +0530

    Features/catalouge page (#23)
    * catalouge page is creating
    
    * css updated
    
    * like button building
    
    * like button done
    
    * filter option added and listing page being processed
    
    * filter funtionality requirement is done
    
    * listing page initiated
    
    * card component updated
    
    * maps
    
    * map added with .env
    
    ---------
    
    Co-authored-by: JeevanKumar R <jeev@JeevanKumars-MacBook-Air.local>

---
+++ src/components/map/Maps.tsx
@@ -0,0 +1,91 @@
+import styles from "@/styles/catalouge.module.css"
+import React, { useState, useEffect, useRef } from "react";
+import { Map , GoogleApiWrapper, Marker } from "google-maps-react-18-support";
+
+interface MapContainerProps  {
+  google: any;
+}
+
+const MapContainer: React.FC<MapContainerProps> = (props) => {
+  const { google } = props;
+  const [markerPosition, setMarkerPosition] = useState<{ lat: number, lng: number }>({ lat: 0, lng: 0 });
+  const mapRef = useRef<any>(null);
+
+  useEffect(() => {
+    // Get the user's current position
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        setMarkerPosition({ lat, lng });
+
+        // Animate the map to the user's current location
+        const map = mapRef.current?.map;
+        if (map) {
+          const newPos = new google.maps.LatLng(lat, lng);
+          map.panTo(newPos);
+        }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }, [google]);
+
+  const handleMapClick = (mapProps: any, map: any, clickEvent: any) => {
+    const lat = clickEvent.latLng.lat();
+    const lng = clickEvent.latLng.lng();
+    setMarkerPosition({ lat, lng });
+  }
+
+  const handleCurrentLocationClick = () => {
+    // Get the user's current position again
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        setMarkerPosition({ lat, lng });
+
+        // Animate the map to the user's current location
+        const map = mapRef.current?.map;
+        if (map) {
+          const newPos = new google.maps.LatLng(lat, lng);
+          map.panTo(newPos);
+        }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  const handleSubmit = () => {
+    console.log(`Latitude: ${markerPosition.lat}, Longitude: ${markerPosition.lng}`);
+  }
+
+  return (
+    <div className={styles.maps}>
+      <div className={styles.maps}>
+        {/* @ts-ignore */}
+      <Map
+        google={google}
+        zoom={8}
+        initialCenter={markerPosition}
+        onClick={handleMapClick}
+        ref={mapRef}
+      >
+        <Marker position={markerPosition} />
+      </Map>
+      </div>
+      <div className="flex">
+         <button onClick={handleCurrentLocationClick} className="btn btn-primary ml-4">Current Location</button>
+         <button onClick={handleSubmit} className="btn btn-primary ml-4">Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey:process.env.API_KEY,
+})(MapContainer);
+
